ef7546893903103653531adb09a25a86f4106983
kazuki-b <kzkzkzk815@icloud.com>
2020-09-11 18:26:04 +0900

    presence一括設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
   # has_many :items
   # has_many :purchases
 
-  
-  
-  validates :nickname,        presence: true
-  validates :email,           presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
-  validates :password,        presence: true, length: { minimum: 6 }, confirmation: true, format: { with: /\A[a-z0-9]+\z/i } 
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname,
+    validates :email,            uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
+    validates :password,         length: { minimum: 6 }, confirmation: true, format: { with: /\A[a-z0-9]+\z/i } 
+    validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday,
+  end
 end
