788ae7c6122a5b2c1cdc364b57b667c314cb19af
kazuki-b <kzkzkzk815@icloud.com>
2020-09-12 00:17:53 +0900

    デフォルトのパスワードに英数混在設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {"yamada"}
     email                 {"kkk@gmail.com"}
-    password              {"000000"}
+    password              {"000000ss"}
     password_confirmation {password}
     last_name             {"山田"}
     first_name            {"太郎"}
