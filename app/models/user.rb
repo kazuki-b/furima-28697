a45b1ce2a75da54969cb6697381666bd3d9b67aa
kazuki-b <kzkzkzk815@icloud.com>
2020-09-11 15:30:24 +0900

    deviseのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_many :items
+  # has_many :purchases
+
+  
+  
+  validates :nickname,        presence: true
+  validates :email,           presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
+  validates :password,        presence: true, length: { minimum: 6 }, confirmation: true, format: { with: /\A[a-z0-9]+\z/i } 
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,        presence: true
 end
