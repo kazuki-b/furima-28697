a0f99f572283b680504de63e1425624b17c45e77
kazuki-b <kzkzkzk815@icloud.com>
2020-09-11 15:32:22 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'tops/index'
+  root to: "tops#index"
   devise_for :users
-  root to:"tops#index"
+  resources :tops
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
