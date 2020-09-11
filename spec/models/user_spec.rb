da3cc087b16ceca28216a61f7bd2e0f2290849ce
kazuki-b <kzkzkzk815@icloud.com>
2020-09-12 00:18:46 +0900

    テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it "passwordが6文字以上で登録できる" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
+        @user.password = "000000ss"
+        @user.password_confirmation = "000000ss"
         expect(@user).to be_valid
       end
       it "passwordが半角英数字混合で登録できる" do
@@ -81,12 +81,51 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "メールアドレスが一意出ないと登録できない" do
+      it "メールアドレスが一意でないと登録できない" do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "メールアドレスに＠が含まれていないと登録が出来ない" do
+        @user.email = "kkkk.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it "苗字、名前が全角（漢字・ひらがな・カタカナ）以外は登録が出来ない" do
+        @user.last_name = "ﾃﾗﾀﾞ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+      it "名前が全角（漢字・ひらがな・カタカナ）以外は登録が出来ない" do
+        @user.first_name = "ﾀﾛｳ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+      it "苗字のフリガナは全角カタカナ以外は登録できない" do
+        @user.last_name_kana = "山田たろう"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
+      it "名前のフリガナは全角カタカナ以外は登録できない" do
+        @user.first_name_kana = "山田たろう"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+      it "passwordが5文字以下では登録できない" do
+        @user.password = "000ss"
+        @user.password_confirmation = "000ss"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)",
+        "Password is too short (minimum is 6 characters)",
+        "Password confirmation is too short (minimum is 6 characters)")
+      end
+      it "passwordが半角英数字混在でなければ登録できない" do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は英字と数字両方を含むパスワードを設定してください", "Password confirmation は英字と数字両方を含むパスワードを設定してください")
       end
     end
   end
