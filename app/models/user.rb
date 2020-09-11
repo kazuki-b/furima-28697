6451ce61dd9814e1f105b9ebbde45353e2dc2d48
kazuki-b <kzkzkzk815@icloud.com>
2020-09-12 00:17:09 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,18 @@ class User < ApplicationRecord
   # has_many :items
   # has_many :purchases
 
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
   with_options presence: true do
-    validates :nickname,
-    validates :email,            uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
-    validates :password,         length: { minimum: 6 }, confirmation: true, format: { with: /\A[a-z0-9]+\z/i } 
-    validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :birthday,
+    validates :nickname
+    validates :email,                 uniqueness: true, format: { with: EMAIL_REGEX,message: 'のフォーマットが不適切です'}
+    validates :password,              length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください' } 
+    validates :password_confirmation, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください' } 
+    validates :last_name,             format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,            format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana,        format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana,       format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
   end
 end
