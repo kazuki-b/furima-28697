ffc8746689905b7795ac79677824093014c55bf9
kazuki-b <kzkzkzk815@icloud.com>
2020-09-14 17:15:25 +0900

    itemモデルのバリデーション

---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :area
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :status
+  belongs_to_active_hash :ship_date
+  belongs_to             :user, optional: true
+  has_one_attached       :image
+  # has_one    :purcha
+
+  with_options presence: true do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :area_id
+    validates :ship_date_id
+    validates :name
+    validates :comment
+    validates :price,                  inclusion: 300..9999999
+    validates :image
+    validates :user_id
+  end
+
+  validates :category_id,numericality: { other_than: 1 }
+  validates :status_id,numericality: { other_than: 1 }
+  validates :area_id,numericality: { other_than: 1 }
+  validates :postage_id,numericality: { other_than: 1 }
+  validates :ship_date_id, numericality: { other_than: 1 }
+  
+end
