c3af25226b5620baf272ddfea12d7214ab4835be
kazuki-b <kzkzkzk815@icloud.com>
2020-09-14 18:05:53 +0900

    active_hashのidが1では出品できない

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,6 +61,31 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship date can't be blank", "Ship date is not a number")
       end
+      it "商品カテゴリが---では出品できない" do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "商品状態が空では---できない" do
+        @item.status_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it "配送料が---では出品できない" do
+        @item.postage_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage must be other than 1")
+      end
+      it "発送元が---では出品できない" do
+        @item.area_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it "発送日が---では出品できない" do
+        @item.ship_date_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship date must be other than 1")
+      end
       it "価格が空では出品できない" do
         @item.price = ""
         @item.valid?
