08b57dadff274b505188b00fb507afe46569cb6f
kazuki-b <kzkzkzk815@icloud.com>
2020-09-16 11:44:05 +0900

    スペル間違い

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:show]
-  before_action :set_item, only:[:show,:edit,:update,:dsetroy]
+  before_action :set_item, only:[:show,:edit,:update,:destroy]
 
   def new
     @item = Item.new
