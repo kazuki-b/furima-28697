d6eb2b409af151200612beef7649f95f5a582d91
kazuki-b <kzkzkzk815@icloud.com>
2020-09-16 10:51:55 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:show]
-  before_action :set_item
+  before_action :set_item, only:[:show,:edit,:update]
 
   def new
     @item = Item.new
