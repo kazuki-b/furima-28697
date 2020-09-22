27af6f85445fff2db08e209b05117b9125a21207
kazuki-b <kzkzkzk815@icloud.com>
2020-09-22 18:43:47 +0900

    formsへ移動

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,18 +1,5 @@
 class Delivery < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :purchase
   belongs_to_active_hash :area
-
-  with_options presence: true do
-    validates :postal_cord, format:{with: /\A\d{3}[-]\d{4}\z/ }
-    validates :city
-    validates :address
-    validates :building_name
-    validates :phone_number, format:{with:/\A\d{10,11}\z/}
-    validates :area_id
-    validates :purchase_id
-  end
-
-  validates :area_id, numericality: { other_than: 1 }
 end
-
-
